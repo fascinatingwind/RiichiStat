d2cedd32ef3e924b460a439524c186be586e7784
Ludvikovskiy, Evgeniy <ludvikovskiy.ev@stratasolutions.ru>
2023-12-17 17:46:28 +0300

    add setters

--- MahjongStatistic/include/mahjong.h
+++ MahjongStatistic/include/mahjong.h
@@ -19,20 +19,21 @@ private:
 public:
     //инициализация известных тайлов 
     void defineHand();
-    void setHand();
-    void setDeclaredTiles();
+    void setHand(std::vector<Tile> hand_);
+    void setDeclaredTiles(std::vector<Tile> declared_tiles_);
 };
 
 class Mahjong
 {
 private:
     Gamer east_gamer, soutn_gamer, west_gamer, north_gamer;
-    std::vector<Tile> wall;
+    std::vector<Tile> center;
+    int wall_size;
 public:
     //выдает вероятность вытянуть из стены заданный тайл  
     double getStatistics(int id);
     //задать состояние игры в конкретный момент времени, без учета предыдущих действий
-    void setCondition();
+    void setCondition(std::vector<std::vector<Tile>> hands, std::vector<std::vector<Tile>> declared_tiles, std::vector<Tile> center_, int wall_size_);
     //void changeGamer();
     void runGame();
 };
--- MahjongStatistic/src/mahjong.cpp
+++ MahjongStatistic/src/mahjong.cpp
@@ -1,1 +1,25 @@
 #include "mahjong.h"
+
+void Mahjong::setCondition(std::vector<std::vector<Tile>> hands
+                            , std::vector<std::vector<Tile>> declared_tiles
+                            , std::vector<Tile> center_
+                            , int wall_size_)  {
+    east_gamer.setHand(hands[0]);
+    east_gamer.setDeclaredTiles(declared_tiles[0]);
+    north_gamer.setHand(hands[1]);
+    north_gamer.setDeclaredTiles(declared_tiles[1]);
+    soutn_gamer.setHand(hands[2]);
+    soutn_gamer.setDeclaredTiles(declared_tiles[2]);
+    west_gamer.setHand(hands[3]);
+    west_gamer.setDeclaredTiles(declared_tiles[3]);
+
+    center = center_;
+    wall_size = wall_size_;
+}
+
+void Gamer::setHand(std::vector<Tile> hand_) {
+    hand = hand_;
+}
+void Gamer::setDeclaredTiles(std::vector<Tile> declared_tiles_) {
+    declared_tiles = declared_tiles_;
+}
